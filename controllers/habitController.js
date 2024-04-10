cfdc7b52d7d59f8e4d586f923c3e40322cf659bc
egorova-sofya <egorovaSjob@yandex.ru>
2024-04-10 16:28:58 +0300

    added env variables

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -4,19 +4,30 @@ const habits = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/habits.json`)
 );
 
+exports.checkID = (req, res, next, id) => {
+  if (id * 1 > habits.length) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "Habit not found" });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  console.log("req", req);
+  if (!req.body.title || !req.body.color) {
+    return res.status(400).json({ status: "error", message: "Bad request" });
+  }
+  next();
+};
+
 exports.getAllHabits = (req, res) => {
-  console.log(req.requestTime);
   res.status(200).json({ status: "success", results: habits.length, habits });
 };
 
 exports.getHabit = (req, res) => {
   const id = req.params.id;
   const habit = habits.find((item) => item.id === Number(id));
-  if (!habit) {
-    return res
-      .status(404)
-      .json({ status: "error", message: "Habit not found" });
-  }
   res.status(200).json({ status: "success", habit });
 };
 
--- routes/habitRoutes.js
+++ routes/habitRoutes.js
@@ -5,12 +5,17 @@ const {
   getHabit,
   editHabit,
   deleteHabit,
+  checkID,
+  checkBody,
 } = require("../controllers/habitController");
 
 const router = express.Router();
 
+// param - миддлвар, который позволяет получать параметры из url. Если в url нет параметра id - миддлвар будет проигнорирован
+router.param("id", checkID);
+
 //route  позволяет не дублировать url /api/v1/habits
-router.route("/").get(getAllHabits).post(addHabit);
+router.route("/").get(getAllHabits).post(checkBody, addHabit);
 router.route("/:id").get(getHabit).patch(editHabit).delete(deleteHabit);
 
 module.exports = router;
